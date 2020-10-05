c740876fda276779ecb6f8b9e61b6f89872f3053
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-05 11:57:33 +0300

    show all data fields of repository

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 const RepositoryItem = (repository) => {
+	const repositoryObject = repository.repository;
+	console.log('repositoryObject', repositoryObject);
 	return (
-	<Text>{repository.repository.description}</Text>
+		<View>
+			<Text>Full name: {repositoryObject.fullName}</Text>
+			<Text>Description: {repositoryObject.description}</Text>
+			<Text>Language: {repositoryObject.language}</Text>
+			<Text>Stars: {repositoryObject.stargazersCount}</Text>
+			<Text>Forks: {repositoryObject.forksCount}</Text>
+			<Text>Reviews: {repositoryObject.reviewCount}</Text>
+			<Text>Rating: {repositoryObject.ratingAverage}</Text>
+		</View>
 	);
 };
 
