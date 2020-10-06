23be165f80dae1023b48c75ff0a55766bbd64909
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-06 12:32:08 +0300

    install router and create tab for signin component

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
 
+import { Link } from 'react-router-native';
+import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#24292e'
   }
 });
 
-const AppBarComponent = () => {
+const AppBar = () => {
 return (
   <View>
     <Appbar.Header style={styles.container}>
-      <Appbar.Content title= "Repositories" />
+      <Appbar.Content title= "Repositories">
+        <Link to="/" component={RepositoryList} />
+      </Appbar.Content>
+      <Appbar.Content title= "Sign in">
+        <Link to="/signin" component={SignIn} />
+      </Appbar.Content>
     </Appbar.Header>
   </View>
 );
 };
 
-export default AppBarComponent;
+export default AppBar;
