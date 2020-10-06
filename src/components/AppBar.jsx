299505b85c1a5ab45051283955de78be63e3d17e
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-06 13:08:56 +0300

    add TouchableWithoutFeedback

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
 
-import { Link } from 'react-router-native';
+import { Link, TouchableWithoutFeedBack } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import SignIn from './SignIn';
 
@@ -16,12 +16,20 @@ const AppBar = () => {
 return (
   <View>
     <Appbar.Header style={styles.container}>
-      <Appbar.Content title= "Repositories">
-        <Link to="/" component={RepositoryList} />
-      </Appbar.Content>
-      <Appbar.Content title= "Sign in">
-        <Link to="/signin" component={SignIn} />
-      </Appbar.Content>
+    <TouchableWithoutFeedBack>
+      <View>
+        <Appbar.Content title= "Repositories">
+          <Link to="/" component={RepositoryList} />
+        </Appbar.Content>
+      </View>
+    </TouchableWithoutFeedBack>
+      <TouchableWithoutFeedBack>
+        <View>
+          <Appbar.Content title= "Sign in">
+            <Link to="/signin" component={SignIn} />
+          </Appbar.Content>
+        </View>
+      </TouchableWithoutFeedBack>
     </Appbar.Header>
   </View>
 );
