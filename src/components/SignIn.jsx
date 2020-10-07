99eb9fa831dd8e33a369f8b92964257da3b96908
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-07 12:58:37 +0300

    create signin form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Formik } from 'formik';
+import { Button, View } from 'react-native';
+
+import FormikTextInput from './FormikTextInput';
+
+const initialValues = {
+  username: '',
+  password: ''
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+        <FormikTextInput name="username" placeholder="Username"/>
+        <FormikTextInput name="password" placeholder="Password"/>
+        <Button onPress={onSubmit} title="Log in"/>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = values => {
+    console.log('values', values);
+  };
+
+  return (
+    <View>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
