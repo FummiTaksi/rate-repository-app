d989c85d1fd511b2d1ae843a754217fec8280d2c
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-08 15:24:21 +0300

    add graphql directory with query

---
+++ src/graphql/queries.js
@@ -0,0 +1,23 @@
+import { gql } from 'apollo-boost';
+
+export const GET_REPOSITORIES = gql`
+query {
+  repositories {
+    pageInfo {
+      totalCount
+    }
+    edges {
+      node {
+        id
+        description
+        ratingAverage
+        reviewCount
+        ownerAvatarUrl
+        fullName
+        language
+        stargazersCount
+      }
+    }
+  }
+}
+`;
