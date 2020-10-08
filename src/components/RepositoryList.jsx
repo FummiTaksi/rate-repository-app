a1366c96bc85acfe2cdb31d9caa05e48c4913af4
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-08 08:16:24 +0300

    extract data fetching to hooks directory

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 
 import RepositoryItem from './RepositoryItem';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -12,22 +13,12 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.100.13:5000/api/repositories');
-    const json = await response.json();
-    console.log(json);
-    setRepositories(json);
-  };
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
+  const { repositories } = useRepositories();
 
   const repositoryNodes = repositories
   ? repositories.edges.map(edge => edge.node)
   : [];
+
   return (
     <FlatList
       data={repositoryNodes}
