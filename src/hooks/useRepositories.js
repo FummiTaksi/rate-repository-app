c6ec36b6c6eeb3f6d882a2f70ac9b04fc3ad806f
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-13 11:27:59 +0300

    add fetchPolicy

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,7 +2,9 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const response = useQuery(GET_REPOSITORIES);
+  const response = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  });
   return { data: response.data , loading: response.loading};
 };
 
