f20b915943ca646376596ac929e1c8b229313077
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-14 13:51:57 +0300

    call mutate with aWAIT

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
+
 import { useMutation } from '@apollo/client';
 import { AUTHORIZE } from '../graphql/mutations';
 
+import AuthStorageContext from '../contexts/AuthStorageContext';
+
 
 const useSignIn = () => {
+	const authStorage = useContext(AuthStorageContext);
   const [mutate, result] = useMutation(AUTHORIZE);
   
   const signIn = async ({ username, password }) => {
-		mutate({ variables: { credentials: { username: username,  password: password }}});
+		await mutate({ variables: { credentials: { username: username,  password: password }}});
 		return result;
   };
   return [signIn, result];
