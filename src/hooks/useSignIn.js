a775649e499e34b96eb14e45ca40fc70aa21092d
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-10-14 14:17:30 +0300

    call setAccessToken when sign in

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,17 +1,18 @@
 import { useContext } from 'react';
+import { useMutation, useApolloClient } from '@apollo/client';
 
-import { useMutation } from '@apollo/client';
 import { AUTHORIZE } from '../graphql/mutations';
-
 import AuthStorageContext from '../contexts/AuthStorageContext';
-
 
 const useSignIn = () => {
 	const authStorage = useContext(AuthStorageContext);
+	const apolloClient = useApolloClient();
   const [mutate, result] = useMutation(AUTHORIZE);
   
   const signIn = async ({ username, password }) => {
-		await mutate({ variables: { credentials: { username: username,  password: password }}});
+		const { data } = await mutate({ variables: { credentials: { username: username,  password: password }}});
+		await authStorage.setAccessToken(data.authorize.accessToken);
+		apolloClient.resetStore();
 		return result;
   };
   return [signIn, result];
